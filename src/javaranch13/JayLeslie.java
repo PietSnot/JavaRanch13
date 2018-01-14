03167db56b1850eedb29c037207625703078ef05
Piet Snot <pietmuis@gmail.com>
2018-01-14 08:25:52 +0100

    AdventOfCode2017 kijken of dat wil lukken

--- src/javaranch13/JayLeslie.java
+++ src/javaranch13/JayLeslie.java
@@ -15,23 +15,27 @@ import java.util.List;
  */
 public class JayLeslie {
     public static void main(String... args) {
-        int[] a = {10, 11, 12, 9, 9, 8, 8,  14, 15};
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> currentList = new ArrayList<>();
+        Integer[] a = {10, 11, 12, 9, 9, 8, 8,  14, 15};
+        System.out.println(processArray(a));
+    }
+    
+    public static <T extends Comparable<T>> List<List<T>> processArray(T[] a) {
+        List<List<T>> list = new ArrayList<>();
+        List<T> currentList = new ArrayList<>();
         currentList.add(a[0]);
         currentList.add(a[1]);
-        Direction currentDir = a[1] > a[0] ? Direction.UP :
-                               a[1] < a[0] ? Direction.DOWN :
-                                             Direction.EVEN
+        Direction currentDir = a[1].compareTo(a[0]) > 0 ? Direction.UP :
+                               a[1].compareTo(a[0]) < 0 ? Direction.DOWN :
+                                                          Direction.EVEN
         ;
         for (int i = 2; i < a.length; i++) {
-            if (a[i] < a[i - 1] && currentDir == Direction.UP) {
+            if (a[i].compareTo(a[i - 1]) < 0 && currentDir == Direction.UP) {
                 list.add(currentList);
                 currentDir = Direction.DOWN;
                 currentList = new ArrayList<>();
                 currentList.add(a[i]);
             }
-            else if (a[i] > a[i - 1] && currentDir == Direction.DOWN) {
+            else if (a[i].compareTo(a[i - 1]) > 0 && currentDir == Direction.DOWN) {
                 list.add(currentList);
                 currentDir = Direction.UP;
                 currentList = new ArrayList<>();
@@ -42,8 +46,12 @@ public class JayLeslie {
             }
         }
         list.add(currentList);
-        for (int i = 1; i < list.size(); i++) list.get(i).add(0, list.get(i - 1).get(list.get(i-1).size() - 1));
-        System.out.println(list);
+        for (int i = 1; i < list.size(); i++) {
+            List<T> previous = list.get(i - 1);
+            List<T> current = list.get(i);
+            current.add(0, previous.get(previous.size() - 1));
+        }
+        return list;
     }
     
     enum Direction {
